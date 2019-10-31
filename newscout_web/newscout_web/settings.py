09845224e14a488b88694994e5fefbb761963980
Yogesh Panchal <yogesh.panchal@fafadiatech.com>
2019-10-31 17:02:31 +0530

    adchore: ded mongodb database settings for news analytics (#19)

--- newscout_web/newscout_web/settings.py
+++ newscout_web/newscout_web/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from pymongo import MongoClient
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -172,3 +173,7 @@ EMAIL_TIMEOUT = None
 
 ELASTIC_SERVER_IP="localhost"
 ELASTIC_SERVER_PORT="9200"
+
+DBNAME = 'newscout_analytics'
+client = MongoClient()
+DB = client[DBNAME]
