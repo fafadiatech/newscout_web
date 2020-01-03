dbe5870e23ad78e99a082c6ad84033f7cadc71b1
hardik <hardik@fafadiatech.com>
2020-01-03 17:40:10 +0530

    added default image in article latestnews page

--- newscout_web/frontend/src/app/NewsApp/LatestNews.js
+++ newscout_web/frontend/src/app/NewsApp/LatestNews.js
@@ -12,6 +12,8 @@ import 'newscout/assets/CardItem.css'
 import 'newscout/assets/SectionTitle.css'
 import 'newscout/assets/Sidebar.css'
 
+import config_data from './config.json';
+
 const DOMAIN = "domain=newscout";
 const URL = "/news/search/";
 
@@ -75,7 +77,11 @@ class LatestNews extends React.Component {
 			article_dict['source'] = item.source
 			article_dict['source_url'] = item.source_url
 			article_dict['date'] = moment(item.published_on).format('YYYY-MM-DD');
-			article_dict['src'] = "http://images.newscout.in/unsafe/336x150/left/top/"+decodeURIComponent(item.cover_image)
+			if(item.cover_image){
+				article_dict['src'] = "http://images.newscout.in/unsafe/336x150/left/top/"+decodeURIComponent(item.cover_image)
+			} else {
+				article_dict['src'] = "http://images.newscout.in/unsafe/336x150/left/top/"+config_data.defaultImage;
+			}
 			latestnews_array.push(article_dict)
 		})
 		this.setState({
