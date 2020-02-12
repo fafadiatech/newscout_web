276834dda4fa6428bc7667fe19e647c48e065ed5
hardik <hardik@fafadiatech.com>
2020-02-12 19:46:52 +0530

    added news detail page ui

--- newscout_web/frontend/src/app/NewsApp/ArticleDetail.js
+++ newscout_web/frontend/src/app/NewsApp/ArticleDetail.js
@@ -2,14 +2,14 @@ import React from 'react';
 import moment from 'moment';
 import logo from './logo.png';
 import ReactDOM from 'react-dom';
-import { CardItem, Menu, SectionTitle, SideBox } from 'newscout';
+import { JumboBox, Menu, SideBox } from 'newscout';
 
 import { MENUS, ARTICLE_DETAIL_URL } from '../../utils/Constants';
 import { getRequest } from '../../utils/Utils';
 
 import 'newscout/assets/Menu.css'
-import 'newscout/assets/CardItem.css'
-import 'newscout/assets/SectionTitle.css'
+import 'newscout/assets/JumboBox.css'
+import 'newscout/assets/ImageOverlay.css'
 import 'newscout/assets/SideBox.css'
 
 import config_data from './config.json';
@@ -31,13 +31,17 @@ class ArticleDetail extends React.Component {
 
 	getArticleDetail = (data) => {
 		var state = this.state;
+		var article_dict = {}
 		state.article.id = data.body.article.id;
+		state.article.slug = data.body.article.slug;
 		state.article.altText = data.body.article.title;
 		state.article.header = data.body.article.title;
 		state.article.caption = data.body.article.blurb;
 		state.article.source = data.body.article.source;
 		state.article.source_url = data.body.article.source_url;
-		state.article.date = moment(data.body.article.published_on).format('YYYY-MM-DD');
+		state.article.category = data.body.article.category;
+		state.article.hash_tags = data.body.article.hash_tags;
+		state.article.date = moment(data.body.article.published_on).format('DD-MM-YYYY');
 		if(data.body.article.cover_image){
 			state.article.src = "http://images.newscout.in/unsafe/1080x610/smart/"+decodeURIComponent(data.body.article.cover_image);
 		} else {
@@ -50,20 +54,17 @@ class ArticleDetail extends React.Component {
 	getRecommendationsResults = (data) => {
 		var recommendations_array = []
 		data.body.results.map((item, index) => {
-			var article_dict = {}
-			article_dict['id'] = item.id
-			article_dict['header'] = item.title
-			article_dict['altText'] = item.title
-			article_dict['caption'] = item.blurb
-			article_dict['source'] = item.source
-			article_dict['url'] = item.source_url
 			if(item.cover_image){
+				var article_dict = {}
+				article_dict['id'] = item.id
+				article_dict['header'] = item.title
+				article_dict['altText'] = item.title
+				article_dict['slug'] = "/news/article/"+item.slug
+				article_dict['published_on'] = moment(item.published_on).format('DD-MM-YYYY');
 				article_dict['src'] = "http://images.newscout.in/unsafe/150x80/left/top/"+decodeURIComponent(item.cover_image)
-			} else {
-				article_dict['src'] = "http://images.newscout.in/unsafe/fit-in/150x80/left/top/"+config_data.defaultImage
-			}
-			if(recommendations_array.length < 5){
-				recommendations_array.push(article_dict)
+				if(recommendations_array.length < 5){
+					recommendations_array.push(article_dict)
+				}
 			}
 		})
 		this.setState({
@@ -94,29 +95,29 @@ class ArticleDetail extends React.Component {
 
 	render() {
 		var { menus, article, recommendations } = this.state;
-
 		return(
 			<React.Fragment>
 				<Menu logo={logo} navitems={menus} url={URL} isSlider={false} />
 				<div className="pt-70">
-					<div className="container-fluid">
+					<div className="container">
 						<div className="row">
-							<div className="col-lg-7 offset-lg-1 col-12 mb-4">
-								<div className="card-container-detail">
-									<CardItem 
+							<div className="col-lg-8 col-12 mb-4">
+								<div className="article-detail">
+									<JumboBox 
+										source_url={article.source_url}
 										image={article.src}
 										title={article.header}
-										description={article.caption}
 										uploaded_on={article.date}
 										uploaded_by={article.source}
-										posturl={article.url}
-										source_url={article.source_url}
-										posturl={article.source_url} />
+										description={article.caption}
+										hash_tags={article.hash_tags} />
 								</div>
 							</div>
-							<div className="col-lg-3 col-12 mb-4">
-								<div className="side-box">
-									<SectionTitle title="More News" />
+							<div className="col-lg-4 col-12 mb-4">
+								<div className="sidebox">
+									<div className="heading">
+										<h3 className="text-center">More News</h3>
+									</div>
 									<SideBox posts={recommendations} />
 								</div>
 							</div>
