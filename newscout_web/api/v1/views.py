010ef48d4a4e48ec67eeac96b198c741aadc8be4
rsamantra <rushikeshsamantra328@gmail.com>
2020-03-14 11:13:07 +0530

    Bug: Removed authentication permission in daily digest

--- newscout_web/api/v1/views.py
+++ newscout_web/api/v1/views.py
@@ -1203,7 +1203,7 @@ class CategoryBulkUpdate(APIView):
 
 class GetDailyDigestView(ListAPIView):
     serializer_class = ArticleSerializer
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (AllowAny)
 
     def format_response(self, response):
         results = []
