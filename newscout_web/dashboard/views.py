69bc596f36ff537e6da7ab956da036636417315f
Yogesh Panchal <yogesh.panchal@fafadiatech.com>
2020-03-16 14:39:34 +0530

    fixed login session issue

--- newscout_web/dashboard/views.py
+++ newscout_web/dashboard/views.py
@@ -6,27 +6,41 @@ from rest_framework.authtoken.models import Token
 from django.contrib.auth import login, authenticate, logout
 from django.views.generic.base import TemplateView, RedirectView
 from braces.views import LoginRequiredMixin
-
+from django.contrib.auth.views import redirect_to_login
 from .forms import LoginForm
 
 
-class EditorTemplateView(TemplateView):
+class EditorTemplateView(LoginRequiredMixin, TemplateView):
     """
     custom view to restrict view only for news editors
     """
 
     def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect_to_login(request.get_full_path(),
+                                         self.get_login_url(),
+                                         self.get_redirect_field_name())
+
         if not request.user.is_editor:
             return HttpResponseRedirect("/")
         return super(EditorTemplateView, self).dispatch(
             request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        res = self.render_to_response(context)
+        token = request.COOKIES.get("token")
+        if not token:
+            token, _ = Token.objects.get_or_create(user=request.user)
+            res.set_cookie("token", token)
+        return res
 
 
 class MainIndexView(TemplateView):
     	template_name = "index.html"
 
 
-class IndexView(LoginRequiredMixin, EditorTemplateView):
+class IndexView(EditorTemplateView):
     template_name = "dashboard-index.html"
 
     def get_context_data(self, **kwargs):
