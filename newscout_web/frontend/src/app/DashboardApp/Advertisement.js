c44194ab5622e50f5e46d1dafe0db355c8c58815
WitchKing's <witchkings@WitchKingss-MacBook-Pro.local>
2020-04-07 14:47:59 +0530

    fixed issue with the regex for advert url

--- newscout_web/frontend/src/app/DashboardApp/Advertisement.js
+++ newscout_web/frontend/src/app/DashboardApp/Advertisement.js
@@ -56,7 +56,7 @@ class Advertisement extends React.Component {
 			errors["ad_url"] = required_msg;
 		} else if (!this.valid_url(fields["ad_url"])) {
 			formIsValid = false;
-			errors["ad_url"] = "Not a valid url eg: http://example.com";
+			errors["ad_url"] = "Not a valid url eg: https://example.com";
 		}
 
 		this.setState({ errors: errors });
@@ -64,7 +64,7 @@ class Advertisement extends React.Component {
 	}
 
 	valid_url = (url) => {
-		var regexp = /^(https:\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+		var regexp = /^(https:\/\/)|(http:\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
 		if (regexp.test(url)) {
 			return true;
 		} else {
@@ -278,7 +278,7 @@ class Advertisement extends React.Component {
 	}
 
 	isSideBarToogle = (data) => {
-		if(data === true){
+		if (data === true) {
 			this.setState({ isSideOpen: true })
 			cookies.set('isSideOpen', true, { path: '/' });
 		} else {
@@ -292,7 +292,7 @@ class Advertisement extends React.Component {
 		this.getAdvertisements()
 		var group_type_url = GROUP_GROUPTYPE_URL;
 		getRequest(group_type_url, this.getGroupAndGroupType, authHeaders)
-		if(cookies.get('isSideOpen')){
+		if (cookies.get('isSideOpen')) {
 			this.setState({ isSideOpen: true })
 		} else {
 			this.setState({ isSideOpen: false })
