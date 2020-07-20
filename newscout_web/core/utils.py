d753b67d64eeb05541e3ab6af35c38494a0a01eb
Sidharth Shah <sidharth@fafadiatech.com>
2020-07-20 19:44:42 +0530

    ref #476: implemented related queries API

--- newscout_web/core/utils.py
+++ newscout_web/core/utils.py
@@ -7,13 +7,9 @@ from elasticsearch import Elasticsearch
 
 
 es = Elasticsearch(
-    hosts=[
-        {
-            'host': settings.ELASTIC_SERVER_IP,
-            'port': settings.ELASTIC_SERVER_PORT
-        }
-    ]
+    hosts=[{"host": settings.ELASTIC_SERVER_IP, "port": settings.ELASTIC_SERVER_PORT}]
 )
+
 
 def create_index(index, mapping=None):
     """
@@ -24,9 +20,9 @@ def create_index(index, mapping=None):
             es.indices.create(index=index, body=mapping)
         else:
             es.indices.create(index=index)
-        print ("Created Index >>>>>>>>>>> " + index)
+        print("Created Index >>>>>>>>>>> " + index)
     else:
-        print ("Index Already exsits >>>>>>>>>>> " + index)
+        print("Index Already exsits >>>>>>>>>>> " + index)
 
 
 def delete_existing_index(index):
@@ -36,9 +32,9 @@ def delete_existing_index(index):
     """
     if es.indices.exists(index):
         es.indices.delete(index=index)
-        print ("Deleted Index >>>>>>>>>>> " + index)
+        print("Deleted Index >>>>>>>>>>> " + index)
     es.indices.create(index=index)
-    print ("Created Index >>>>>>>>>>> " + index)
+    print("Created Index >>>>>>>>>>> " + index)
 
 
 def create_mapping_for_index(index, mapping):
@@ -48,9 +44,9 @@ def create_mapping_for_index(index, mapping):
     """
     if es.indices.exists(index):
         es.indices.delete(index=index)
-        print ("Deleted Index >>>>>>>>>>> " + index)
+        print("Deleted Index >>>>>>>>>>> " + index)
     es.indices.create(index=index, body=mapping)
-    print ("Created Index >>>>>>>>>>> " + index)
+    print("Created Index >>>>>>>>>>> " + index)
 
 
 def ingest_to_elastic(docs, index, item_type, item_id):
@@ -60,7 +56,7 @@ def ingest_to_elastic(docs, index, item_type, item_id):
         action = {
             "_index": index,
             "_id": md5(str(item[item_id]).encode()).hexdigest(),
-            "_source": item
+            "_source": item,
         }
         if item_type:
             action["_type"] = item_type
@@ -77,7 +73,7 @@ def update_to_elastic(docs, index, item_type, item_id):
             "_index": index,
             "_type": item_type,
             "_id": md5(str(item[item_id]).encode()).hexdigest(),
-            "doc": item
+            "doc": item,
         }
         actions.append(action)
     helpers.bulk(es, actions, chunk_size=100, request_timeout=20)
@@ -91,7 +87,29 @@ def delete_from_elastic(docs, index, item_type, item_id):
             "_index": index,
             "_type": item_type,
             "_id": md5(str(item[item_id]).encode()).hexdigest(),
-            "doc": item
+            "doc": item,
         }
         actions.append(action)
     helpers.bulk(es, actions, index=index, refresh=True)
+
+
+def get_docs(index, start=0, size=25):
+    results = []
+    response = es.search(
+        index=index, body={"from": start, "size": size, "query": {"match_all": {}}}
+    )
+    if "hits" in response and "hits" in response["hits"]:
+        for current in response["hits"]["hits"]:
+            results.append(current["_source"]["desc"])
+    return results, response["hits"]["total"]
+
+
+def search_index(q, index, fields, start=0, size=100):
+    results = []
+    response = es.search(
+        index=index, body={"_source": fields, "from": start, "size": size, "query": q}
+    )
+    if "hits" in response and "hits" in response["hits"]:
+        for current in response["hits"]["hits"]:
+            results.append(current["_source"])
+    return results, response["hits"]["total"]
