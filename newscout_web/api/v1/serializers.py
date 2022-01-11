a3abd5c3352cabf9b8e41c9a585ee535ccc7962b
Arch-020 <archana.gawari@fafadiatech.com>
2022-01-11 09:42:54 +0000

    minor changes

--- newscout_web/api/v1/serializers.py
+++ newscout_web/api/v1/serializers.py
@@ -127,18 +127,18 @@ class ArticleSerializer(serializers.ModelSerializer):
         return ""
 
     def get_root_category(self, instance):
-        # if instance.category.name not in ["Uncategorised", "Uncategorized"]:
-        #     root_category = CategoryAssociation.objects.get(child_cat=instance.category)
-        #     return root_category.parent_cat.name
-        # return instance.category.name
-        return "Arts & Culture"
+        if instance.category.name not in ["Uncategorised", "Uncategorized"]:
+            root_category = CategoryAssociation.objects.get(child_cat=instance.category)
+            return root_category.parent_cat.name
+        return instance.category.name
+        
 
     def get_root_category_id(self, instance):
-        # if instance.category.name not in ["Uncategorised", "Uncategorized"]:
-        #     root_category = CategoryAssociation.objects.get(child_cat=instance.category)
-        #     return root_category.parent_cat_id
-        # return instance.category.id
-        return 637
+        if instance.category.name not in ["Uncategorised", "Uncategorized"]:
+            root_category = CategoryAssociation.objects.get(child_cat=instance.category)
+            return root_category.parent_cat_id
+        return instance.category.id
+        
 
 
 class UserSerializer(serializers.Serializer):
