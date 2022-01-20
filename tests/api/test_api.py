3263af09f32e2468aaddd947c598ead770794963
Arch-020 <archana.gawari@fafadiatech.com>
2022-01-20 06:37:17 +0000

    added tests for recommendations & trending

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -1,14 +1,14 @@
 import json
 import requests
 
-BASE_URL = "http://www.newscout.in/api/v1/articles/"
+BASE_URL = "http://www.newscout.in/api/v1/"
 
 urls = [
-            'going-public-pharmeasy-files-for-rs-6250-crore-ipo-1875720/',
-            'samsung-announces-first-smartphone-chip-with-amd-ray-tracing-gpu-1902898/',
-            'legendary-kathak-dancer-birju-maharaj-dead-1902570/',
-            'satellite-photos-show-aftermath-of-attack-on-abu-dhabi-oil-facility-1902938/',
-            'india-post-payments-bank-customer-base-crosses-5-crore-mark-1902961/'
+            'articles/going-public-pharmeasy-files-for-rs-6250-crore-ipo-1875720/',
+            'articles/samsung-announces-first-smartphone-chip-with-amd-ray-tracing-gpu-1902898/',
+            'articles/legendary-kathak-dancer-birju-maharaj-dead-1902570/',
+            'articles/satellite-photos-show-aftermath-of-attack-on-abu-dhabi-oil-facility-1902938/',
+            'articles/india-post-payments-bank-customer-base-crosses-5-crore-mark-1902961/'
             ]
 
 def test_article_api():
@@ -19,6 +19,26 @@ def test_article_api():
         json_response = json.loads(response.text)
         assert "body" in json_response
         assert "next_article" in json_response["body"]
+
+def test_trending_api():
+    final_url = f"{BASE_URL}trending/?domain=newscout"
+    print(final_url)
+    response = requests.get(final_url)
+    json_response = json.loads(response.text)
+    assert "body" in json_response
+    assert "results" in json_response["body"]
+    for current in json_response["body"]["results"]:
+        assert (current["articles"][0]["id"])
+
+def test_recommendations_api():
+    final_url = f"{BASE_URL}articles/1903574/recommendations/?domain=newscout"
+    print(final_url)
+    response = requests.get(final_url)
+    json_response = json.loads(response.text)
+    assert "body" in json_response
+    assert "results" in json_response["body"]
+    for article in json_response["body"]["results"]:
+        assert (article["id"])
 
 
 if __name__ == "__main__":
